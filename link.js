bfc40c210d9d53e5915f44fc3f69349e254acbd4
RainVisitor <abc873693@gmail.com>
2018-09-24 02:21:29 +0800

    Remove absolute path

--- link.js
+++ link.js
@@ -1,7 +1,7 @@
-﻿document.write("<td width=\"12%\"><div class=\"navigation\"><a href=\"http://whale.cse.nsysu.edu.tw/index.html\" class=\"main_link style19\">首頁</a></div></td>"); 
-document.write("<td width=\"13%\"><div class=\"navigation\"><a href=\"http://whale.cse.nsysu.edu.tw/news.html\" class=\"main_link\">最新消息</a></div></td>"); 
-document.write("<td width=\"15%\"><div class=\"navigation\"><a href=\"http://whale.cse.nsysu.edu.tw/mem.html\" class=\"main_link\">成員</a></div></td>"); 
-document.write("<td width=\"14%\"><div class=\"navigation\"><a href=\"http://whale.cse.nsysu.edu.tw/course.html\" class=\"main_link\">課程訊息</a></div></td>");  
-document.write("<td width=\"12%\"><div class=\"navigation\"><a href=\"http://whale.cse.nsysu.edu.tw/link.html\" class=\"main_link\">相關連結</a></div></td>");       		
-document.write("<td width=\"14%\"><div class=\"navigation\"><a href=\"http://whale.cse.nsysu.edu.tw/labalbum7.html\" class=\"main_link style19\">相簿</a></div></td>"); 
-document.write("<td width=\"20%\"><div class=\"navigation\"><a href=\"http://whale.cse.nsysu.edu.tw/schedule.html\" class=\"main_link style19\">行事曆</a></div></td>"); 
+﻿document.write("<td width=\"12%\"><div class=\"navigation\"><a href=\"/index.html\" class=\"main_link style19\">首頁</a></div></td>"); 
+document.write("<td width=\"13%\"><div class=\"navigation\"><a href=\"/news.html\" class=\"main_link\">最新消息</a></div></td>"); 
+document.write("<td width=\"15%\"><div class=\"navigation\"><a href=\"/mem.html\" class=\"main_link\">成員</a></div></td>"); 
+document.write("<td width=\"14%\"><div class=\"navigation\"><a href=\"/course.html\" class=\"main_link\">課程訊息</a></div></td>");  
+document.write("<td width=\"12%\"><div class=\"navigation\"><a href=\"/link.html\" class=\"main_link\">相關連結</a></div></td>");       		
+document.write("<td width=\"14%\"><div class=\"navigation\"><a href=\"/labalbum7.html\" class=\"main_link style19\">相簿</a></div></td>"); 
+document.write("<td width=\"20%\"><div class=\"navigation\"><a href=\"/schedule.html\" class=\"main_link style19\">行事曆</a></div></td>"); 
